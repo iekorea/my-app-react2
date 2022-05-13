845024fd93bf0670e36bcc1333fbc24496030b3a
iekorea <iekorea@gmail.com>
2022-05-13 16:17:50 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        이 기기의 IP주소는 {ip}입니다.
+        이 기기의 IP주소는 {ip}      입니다!!!!!!!.
       </header>
     </div>
   );
